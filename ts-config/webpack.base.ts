579939f9e40d785646179e1e6cc74d0ee096bd32
pch1024 <pch1024@outlook.com>
2023-12-09 09:25:20 +0800

    update: ci

--- ts-config/webpack.base.ts
+++ ts-config/webpack.base.ts
@@ -195,6 +195,36 @@ const config: Configuration = {
         // nodeEnv: "production",
         moduleIds: "size", // 模块 id 时需要使用哪种算法
         runtimeChunk: "single", // 设置多入口使用一个 runtimeChunk
+        /**
+         * @优化产物大小 配置 optimization.sideEffects 和 TreeShaking，减小产物大小
+         * 告知 webpack 去辨识 package.json 中的副作用标记或规则，
+         * 以跳过那些当导出不被使用且被标记不包含副作用的模块。
+         */
+        sideEffects: true,
+        splitChunks: {
+            /**
+             * @优化渲染速度 使用 splitChunks 分隔公共业务模块，优化编译速度
+             * @优化渲染速度 使用 splitChunks 分隔 node_modules 中变动少的模块，优化编译速度
+             */
+            cacheGroups: {
+                vendors: {
+                    // 提取 node_modules 代码
+                    test: /node_modules/, // 只匹配 node_modules 里面的模块
+                    name: "vendors", // 提取文件命名为 vendors,js后缀和 chunkhash 会自动加
+                    minChunks: 1, // 只要使用一次就提取出来
+                    chunks: "initial", // 只提取初始化就能获取到的模块,不管异步的
+                    minSize: 0, // 提取代码体积大于0就提取出来
+                    priority: 1, // 提取优先级为1
+                },
+                commons: {
+                    // 提取公共代码
+                    name: "commons", // 提取文件命名为 commons
+                    minChunks: 2, // 只要使用两次就提取出来
+                    chunks: "initial", // 只提取初始化就能获取到的模块,不管异步的
+                    minSize: 0, // 提取代码体积大于 0 就提取出来
+                },
+            },
+        },
     },
     stats: {
         assetsSort: "size",
